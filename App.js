1d72d603fd43c9fb565ee4a026ed83d86d44d151
Kashyap Vissapragada <vmkashyap1999@gmail.com>
2021-02-25 18:13:05 +0530

    recording audio

--- App.js
+++ App.js
@@ -2,132 +2,125 @@ import { StatusBar } from 'expo-status-bar';
 import React , {useState, useEffect} from 'react';
 import { Alert, Button, Text, TouchableOpacity, TextInput, View, StyleSheet } from 'react-native';
 import { Audio } from 'expo-av';
-
+import * as Permissions from 'expo-permissions';
+import * as FileSystem from 'expo-file-system';
+import axios from "axios";
 
 export default function App() {
 
-  const [isRecording, setIsRecording ] = useState(false);
   const [isFetching, setIsFetching ] = useState(false);
-  const [recording, setRecording ] = useState(null);
   const [email, setEmail] = useState('user');
   const [password, setPassword] = useState('password');
-  const [command, setCommand] = useState(null);
-  const [query, setQuery] = useState(null);
 
-  const recordingOptions = {
-    // android not currently in use, but parameters are required
+  const recordingSettings = {
     android: {
-        extension: '.m4a',
-        outputFormat: Audio.RECORDING_OPTION_ANDROID_OUTPUT_FORMAT_MPEG_4,
-        audioEncoder: Audio.RECORDING_OPTION_ANDROID_AUDIO_ENCODER_AAC,
-        sampleRate: 44100,
-        numberOfChannels: 2,
-        bitRate: 128000,
+      extension: ".mp3",
+      outputFormat: Audio.RECORDING_OPTION_ANDROID_OUTPUT_FORMAT_MPEG_4,
+      audioEncoder: Audio.RECORDING_OPTION_ANDROID_AUDIO_ENCODER_AAC,
+      sampleRate: 44100,
+      numberOfChannels: 2,
+      bitRate: 128000,
     },
     ios: {
-        extension: '.wav',
-        audioQuality: Audio.RECORDING_OPTION_IOS_AUDIO_QUALITY_HIGH,
-        sampleRate: 44100,
-        numberOfChannels: 1,
-        bitRate: 128000,
-        linearPCMBitDepth: 16,
-        linearPCMIsBigEndian: false,
-        linearPCMIsFloat: false,
+      extension: ".mp3",
+      outputFormat: Audio.RECORDING_OPTION_IOS_OUTPUT_FORMAT_MPEG4AAC,
+      audioQuality: Audio.RECORDING_OPTION_IOS_AUDIO_QUALITY_MIN,
+      sampleRate: 44100,
+      numberOfChannels: 2,
+      bitRate: 128000,
+      linearPCMBitDepth: 16,
+      linearPCMIsBigEndian: false,
+      linearPCMIsFloat: false,
     },
-};
+  };
+  
+  const [isRecording, setIsRecording] = useState(false);
+  const [recording, setRecording] = useState(null);
+  const [sound, setSound] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(false);
 
 const ENCODING = 'LINEAR16';
 const SAMPLE_RATE_HERTZ = 41000;
 const LANGUAGE = 'en-US';
-
+  
   const startRecording = async () => {
     const { status } = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
-    if (status !== 'granted') return;
-  
-    setIsRecording(true);
-    // some of these are not applicable, but are required
-    await Audio.setAudioModeAsync({
+    console.log(status);
+    if (status !== 'granted'){
+      throw new Error('Recording permission not granted');
+      return;
+    }
+    // stop playback
+    if (sound !== null) {
+      await sound.unloadAsync();
+      sound.setOnPlaybackStatusUpdate(null);
+      setSound(null);
+    }
 
+    await Audio.setAudioModeAsync({
       allowsRecordingIOS: true,
       interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
       playsInSilentModeIOS: true,
       shouldDuckAndroid: true,
       interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
-      playThroughEarpieceAndroid: true,
-  
+      playThroughEarpieceAndroid: false,
+      staysActiveInBackground: true,
     });
-    const recording = new Audio.Recording();
+    const _recording = new Audio.Recording();
     try {
-      await recording.prepareToRecordAsync(recordingOptions);
-      await recording.startAsync();
+      await _recording.prepareToRecordAsync(recordingSettings);
+      setRecording(_recording);
+      await _recording.startAsync();
+      console.log("recording");
+      setIsRecording(true);
     } catch (error) {
-      console.log(error);
-      stopRecording();
+      console.log("error while recording:", error);
     }
-    setRecording(recording);
-  }
+  };
 
-  const stopRecording = () => {
-    setIsRecording(false);
-  }
-
-  const getTranscription = async () => {
-    setIsFetching(true);
+  const stopRecording = async () => {
     try {
-      const info = await FileSystem.getInfoAsync(recording.getURI());
-      console.log(`FILE INFO: ${JSON.stringify(info)}`);
-      const uri = info.uri;
-      const formData = new FormData();
-      formData.append('file', {
-        uri,
-        type: 'audio/x-wav',
-        // could be anything 
-        name: 'speech2text'
-      });
-      const response = await fetch(config.CLOUD_FUNCTION_URL, {
-        method: 'POST',
-        body: formData
-      });
-      const data = await response.json();
-      setQuery(data.transcript);
-    } catch(error) {
-      console.log('There was an error', error);
-      stopRecording();
-      //resetRecording();
+      await recording.stopAndUnloadAsync();
+    } catch (error) {
+      // Do nothing 
     }
-    setIsFetching(false);
-  }
+    const info = await FileSystem.getInfoAsync(recording.getURI());
+    console.log(`FILE INFO: ${JSON.stringify(info)}`);
 
-  const ids = ["email", "password"];
+    const blob = new Blob([JSON.stringify(info)], {type : 'audio/json'});
+    console.log(blob.blob);
+    var fd = new FormData();
+    fd.append("audio", blob.blob, "audio.mp3");
 
-  useEffect(() => {
-    // Case 1: Select command
-    if (command.startsWith("select")) {
-      let id = command.split("select")[1].trim().toLowerCase();
-      setSelectedId(id);
-      if (document.getElementById(id)) {
-        document.getElementById(id).style.backgroundColor = "#DAF7A6";
+    // axios.post(`${baseURLs[env]}/html/audio`, fd).then((resp) => {
+    //   console.log(resp);
+    //   if (resp.data.transcript) {
+    //     console.log(resp.data.transcript);
+    //   }
+    // });
 
-        for (let remainingId of ids) {
-          if (remainingId !== id) {
-            document.getElementById(remainingId).style.backgroundColor =
-              "transparent";
-          }
-        }
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+      playsInSilentModeIOS: true,
+      playsInSilentLockedModeIOS: true,
+      shouldDuckAndroid: true,
+      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+      playThroughEarpieceAndroid: false,
+      staysActiveInBackground: true,
+    });
+    const { sound: _sound, status } = await recording.createNewLoadedSoundAsync(
+      {
+        isLooping: true,
+        isMuted: false,
+        volume: 1.0,
+        rate: 1.0,
+        shouldCorrectPitch: true,
       }
-    } else if (command.startsWith("insert")) {
-      let value = command.split("insert")[1].trim().toLowerCase();
-      if (document.getElementById(selectedId)) {
-        document.getElementById(selectedId).value = value;
-      }
-    } else if (command.startsWith("clear")) {
-      let id = command.split("clear")[1].trim().toLowerCase();
-
-      if (document.getElementById(id)) {
-        document.getElementById(id).value = "";
-      }
-    }
-  }, [command]);
+    );
+    setSound(_sound);
+    setIsRecording(false);
+  };
   
   const onLogin = () => {
 
@@ -155,11 +148,25 @@ const LANGUAGE = 'en-US';
         />
         
      
-        <TouchableOpacity
+        {/* <TouchableOpacity
           style={styles.button}
           onPress={onLogin}
        >
          <Text style={styles.buttonText}> Log in </Text>
+       </TouchableOpacity> */}
+
+       <TouchableOpacity
+          style={styles.button}
+          onPress={startRecording}
+       >
+         <Text style={styles.buttonText}> Start </Text>
+       </TouchableOpacity>
+
+       <TouchableOpacity
+          style={styles.button}
+          onPress={stopRecording}
+       >
+         <Text style={styles.buttonText}> Stop </Text>
        </TouchableOpacity>
         
     </View>
